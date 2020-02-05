c2800db7dbc33f145f40a6f193f5e9eea4dbb66d
usfbelhadj <usf.belhadj@gmail.com>
2020-02-05 22:28:39 +0000

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "string.h"
+#include "unistd.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (vrai)
+ */
+int main(void)
+{
+int len;
+len = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", len);
+return (1);
+}
