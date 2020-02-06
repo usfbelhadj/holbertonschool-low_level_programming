21c542e3419852e9d2f677df0172505a53116158
usfbelhadj <usf.belhadj@gmail.com>
2020-02-06 21:04:14 +0000

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main - Alphabets
+*Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'A'; c <= 'Z'; ++c)
+	putchar(c);
+return (0);
+}
