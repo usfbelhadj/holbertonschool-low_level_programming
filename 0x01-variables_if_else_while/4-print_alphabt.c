bf527bf25c06a7a599b2f5913e3fc4d5b04f338d
usfbelhadj <usf.belhadj@gmail.com>
2020-02-06 22:34:17 +0000

    im done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 char i;
-for (i = 'a'; i <= 'z'; i++)
+for (i = 'a'; i <= 'z'; ++i)
 {
 if (i != 'e' && i != 'q')
-putchar(i);
+putchar (i);
 }
-putchar("\n");
+putchar ('\n');
 return (0);
 }
