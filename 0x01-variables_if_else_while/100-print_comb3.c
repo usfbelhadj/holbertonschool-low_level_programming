ebf378d468d49bd02f845901c3b7c8152b65d991
usfbelhadj <usf.belhadj@gmail.com>
2020-02-07 19:45:32 +0000

    100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Do
+ *Return: 0
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 10; i++)
+for (j = i + 1; j < 10; j++)
+{
+if (i != j || j != i)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(44);
+putchar(32);
+}
+}
+return (0);
+}
