a53e897b6189ac9d969d13145651093662dd0570
usfbelhadj <usf.belhadj@gmail.com>
2020-02-07 21:41:17 +0000

    am Done

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,11 +14,15 @@ for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
-if (i != 8 || j != 9)
+if (i != 8 && j != 9)
+{
 putchar(44);
 putchar(32);
+}
 }
 }
 putchar('\n');
 return (0);
 }
+
+
