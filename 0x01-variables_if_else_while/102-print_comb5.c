6efbdbe68b800d8b32b5867086b7ff587cd2a165
usfbelhadj <usf.belhadj@gmail.com>
2020-02-07 22:14:36 +0000

    f*ck-it

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,16 +7,16 @@
 */
 int main(void)
 {
-int i,j;
-for (i = 0 ; i < 100; i++)
-for (j = i+1; j < 100; j++)
+int i, j;
+for (i = 0; i < 100; i++)
+for (j = i + 1; j < 100; j++)
 {
 putchar((i / 10) + '0');
 putchar((i % 10) + '0');
 putchar(' ');
 putchar((j / 10) + '0');
 putchar((j % 10) + '0');
-if (i !=98 || j != 99)
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
