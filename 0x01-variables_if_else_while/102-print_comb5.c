ac327e540def86e42ba2f45b050d7a1ba4f7e18c
usfbelhadj <usf.belhadj@gmail.com>
2020-02-07 22:07:09 +0000

    Done-ad

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+*main -Do-it
+*Return: 0
+*/
+int main(void)
+{
+int i,j
+for (i = ; i < 100; i++)
+for (j = i+1; j < 100; j++)
+{
+putchar((i / 10) + '0');
+putchar((i % 10) + '0');
+putchar(' ');
+putchar((j / 10) + '0');
+putchar((j % 10) + '0');
+if (i !=98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar ('\n');
+return (0);
+}
