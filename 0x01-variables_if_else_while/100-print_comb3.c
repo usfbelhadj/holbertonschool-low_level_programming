2a1e5af741906fe9b4830f67be2a96bd7e06c181
usfbelhadj <usf.belhadj@gmail.com>
2020-02-07 21:37:21 +0000

    aaaaa

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,13 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 /**
- *main - Do
- *Return: 0
- */
+*main - Do
+*Return: 0
+*/
 int main(void)
 {
 int i, j;
 for (i = 0; i < 10; i++)
+{
 for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
@@ -16,6 +17,7 @@ putchar(j + '0');
 if (i != 8 || j != 9)
 putchar(44);
 putchar(32);
+}
 }
 putchar('\n');
 return (0);
