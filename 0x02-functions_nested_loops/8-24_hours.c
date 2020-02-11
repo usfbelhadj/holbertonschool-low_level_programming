dd2c94b44f7a9c84f3cf5a7e7f3a683e84ad0802
usfbelhadj <usf.belhadj@gmail.com>
2020-02-11 17:15:18 +0000

    Time

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,14 +7,19 @@ void jack_bauer(void)
 {
 int h, m, h1, h2, m1, m2;
 for (h = 0; h < 24; h++)
+{
 h1 = h / 10;
 h2 = h % 10;
 for (m = 0; m < 60; m++)
+{
 m1 = m / 10;
 m2 = m % 10;
-_putchar(h1);
-_putchar(h2);
+_putchar(h1 + '0');
+_putchar(h2 + '0');
 _putchar(':');
-_putchar(m1);
-_putchar(m2);
+_putchar(m1 + '0');
+_putchar(m2 + '0');
+_putchar('\n');
+}
+}
 }
