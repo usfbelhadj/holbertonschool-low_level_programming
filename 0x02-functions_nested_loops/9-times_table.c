63ed9558f471367f0358ea762ba54d5b28c35d39
usfbelhadj <usf.belhadj@gmail.com>
2020-02-11 17:44:23 +0000

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+*times_table - 9 times table
+*Return: 0
+*/
+void times_table(void)
+{
+int i, j, k;
+for (i = 0; i <= 9; i++)
+{
+_putchar ('0');
+for (j = 1; j <= 9; j++)
+{
+_putchar (',');
+_putchar (' ');
+k = i * j;
+if (k <= 9)
+{
+_putchar (' ');
+_putchar (k + '0');
+}
+else
+{
+_putchar ((k / 10) + '0');
+_putchar ((k % 10) + '0');
+}
+}
+_putchar ('\n');
+}
+}
