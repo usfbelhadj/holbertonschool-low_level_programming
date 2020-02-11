2f888f858fdfd6a2f969f8744e1242ebebdcd748
usfbelhadj <usf.belhadj@gmail.com>
2020-02-11 09:03:39 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,10 @@
 *main - Alphabet
 *Return: 0
 */
-void print_alphabet(void)
+void print_alphabet()
 {
-int i;
-for (i = 'a'; i <= 'z'; ++i);
+char i;
+for (i = 'a'; i <= 'z'; i++);
 _putchar(i);
 _putchar('\n');
-return (0);
 }
