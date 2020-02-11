525b769c38f049003721b19d6e952125f9411964
usfbelhadj <usf.belhadj@gmail.com>
2020-02-11 18:41:27 +0000

    0x03

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
 */
 void times_table(void)
 {
-int i, j, k;
+int i, j;
 for (i = 0; i <= 9; i++)
 {
 _putchar ('0');
@@ -13,16 +13,16 @@ for (j = 1; j <= 9; j++)
 {
 _putchar (',');
 _putchar (' ');
-k = i * j;
-if (k <= 9)
+i * j;
+if (i * j <= 9)
 {
 _putchar (' ');
-_putchar (k + '0');
+_putchar (i * j + '0');
 }
 else
 {
-_putchar ((k / 10) + '0');
-_putchar ((k % 10) + '0');
+_putchar ((i * j / 10) + '0');
+_putchar ((i * j % 10) + '0');
 }
 }
 _putchar ('\n');
