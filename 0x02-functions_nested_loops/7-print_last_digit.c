bb7c6297eaf947d490b6de82d72e1fbb4eda46e2
usfbelhadj <usf.belhadj@gmail.com>
2020-02-11 14:18:41 +0000

    lastDig

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*print_last_digit- last digit
+*@c: string
+*Return: 0
+*/
+int print_last_digit(int c)
+{
+c =(c % 10);
+if (c < 0)
+c = -c;
+_putchar(c + '0');
+return (c);
+}
