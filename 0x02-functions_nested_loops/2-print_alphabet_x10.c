8d4c1b99360c06d065b82416e48b54ebe31e7411
usfbelhadj <usf.belhadj@gmail.com>
2020-02-11 11:18:00 +0000

    Lower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 int j, i;
 for (j = 1; j <= 10; ++j)
 {
-for (i = 97; i <= 122; ++i)
+for (i = 'a'; i <= 'z'; ++i)
 _putchar(i);
 _putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+
+#include "holberton.h"
+/**
+*islower- lower alpha
+*Return: 0
+*/
+int _islower(int c)
+{
+if (c >= 97 && ch <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
