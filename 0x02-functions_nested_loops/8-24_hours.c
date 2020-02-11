48092d4972f8b15be9e4009d1e1eb1c5a6ccb876
usfbelhadj <usf.belhadj@gmail.com>
2020-02-11 17:05:26 +0000

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*jack_bauer- Starting time
+*Return: 0
+*/
+void jack_bauer(void)
+{
+int h, m, h1, h2, m1, m2;
+for (h = 0; h < 24; h++)
+h1 = h / 10;
+h2 = h % 10;
+for (m = 0; m < 60; m++)
+m1 = m / 10;
+m2 = m % 10;
+_putchar(h1);
+_putchar(h2);
+_putchar(':');
+_putchar(m1);
+_putchar(m2);
+}
