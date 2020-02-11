8d973a0f1253c6fca0d7271527d74c8219f1cd20
usfbelhadj <usf.belhadj@gmail.com>
2020-02-11 09:46:47 +0000

    alphab*10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,9 +6,9 @@
 void print_alphabet_x10(void)
 {
 int j, i;
-for (j = 1; j <=10; j++)
+for (j = 1; j <= 10; j++)
 {
-for (i = 97; i <= 122; ++i)
+for (i = 97; i <= 122; i++)
 _putchar(i);
 _putchar('\n');
 }
