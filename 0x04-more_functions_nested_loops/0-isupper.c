5996ac028d695784408508e053fbb32894b0d0de
usfbelhadj <usf.belhadj@gmail.com>
2020-02-13 12:18:18 +0000

    Done

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
- * _isupper- check the code for Holberton School students.
- * Return: Always 0.
- * @c: integer.
+ *_isupper- check the code for Holberton School students.
+ *Return: Always 0.
+ *@c: integer.
  */
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
-return(1);
+return (1);
 else
 return (0);
 }
