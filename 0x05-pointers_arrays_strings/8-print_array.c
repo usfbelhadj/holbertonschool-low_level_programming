4a4dc1e878f8b2b2d30c0a0e0ef17c7e4829cbd8
usfbelhadj <usf.belhadj@gmail.com>
2020-02-18 17:43:55 +0000

    aa

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array -elements of an array of integers
+ *@a: array-int
+ *@n: integer
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+int i = 0;
+while (i < n)
+{
+if (i < n - 1)
+printf("%d, ", a[i]);
+else
+printf("%d\n", a[i]);
+i++;
+}
+}
