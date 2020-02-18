dcf259799c31dbc0bf8f245d69749a59e0bc5ce7
usfbelhadj <usf.belhadj@gmail.com>
2020-02-18 12:51:30 +0000

    aza

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,11 +10,10 @@ int i, j = 0;
 char tmp;
 while (s[j])
 j++;
-j--;
 for (i = 0; i <= j / 2; i++)
 {
-tmp = s[j - i + '\0'];
-s[j - i] = s[i];
+tmp = s[j - i - 1 + '\0'];
+s[j - i - 1] = s[i];
 s[i] = tmp;
 }
 }
