31a243086cb6fd72d60711b49f68f468069299fb
usfbelhadj <56652008+usfbelhadj@users.noreply.github.com>
2020-02-18 23:55:13 +0100

    ss

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,17 +1,15 @@
 #include "holberton.h"
 /**
- * _strcpy - copies the string pointed
+ **_strcpy - copies the string pointed
  *@dest: string
  *@src: source
- *Return: 1
+ *Return: string
  */
 char *_strcpy(char *dest, char *src)
 {
 int i, j = 0;
 while (src[j] != '\0')
-{
 j++;
-}
 for ( i = 0; i <= j; i++)
 {
 dest[i] = src[i];
