e93ba563948223242f722396e63d4d98876613cb
usfbelhadj <usf.belhadj@gmail.com>
2020-02-18 15:39:48 +0000

    aa

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * puts_half- prints half of a string
  *@str: string
@@ -8,11 +7,9 @@
 void puts_half(char *str)
 {
 int i, j, k;
-printf("before while\n");
 j = 0;
 while (str[j] != '\0')
 j++;
-printf("after while\n");
 i = j / 2;
 for (k = i; k <= j - 1; k++)
 _putchar(str[k]);
