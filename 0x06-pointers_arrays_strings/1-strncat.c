a1e5abd9ef197d2e11f195b5509852115a9a4213
usfbelhadj <usf.belhadj@gmail.com>
2020-02-20 16:37:12 +0100

    ss

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_strcat - Concat char
+ *@dest:string
+ *@src:string
+ * Return: strings
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i, j = 0;
+while (dest[j] != '\0')
+{
+j++;
+}
+for (i = 0 ; i < n && src[i] != '\0'; i++)
+{
+dest[j + i] = src[i];
+}
+dest [j + i] = '\0';
+return (dest);
+}
