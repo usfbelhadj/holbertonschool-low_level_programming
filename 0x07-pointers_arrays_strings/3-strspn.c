bed40409f10e9e548ae925206c19361ce7421098
usfbelhadj <usf.belhadj@gmail.com>
2020-02-24 19:33:23 +0100

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,9 +9,9 @@ unsigned int _strspn(char *s, char *accept)
 {
 int i, j, x;
 x = 0;
-for(i = 0; s[i] != '\0'; i++)
+for (i = 0; s[i] != '\0'; i++)
 {
-for(j = 0; accept[j] != '\0'; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
@@ -24,5 +24,5 @@ if (accept[j] == '\0')
 break;
 }
 }
-return(x);
+return (x);
 }
