37da84fd025f17cd7e54661ac13a284d4faf73e3
usfbelhadj <usf.belhadj@gmail.com>
2020-02-25 09:44:37 +0100

    aa

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * _memset - function fills the first bytes
+ * _memset- function fills the first bytes
  * @s: the address of memory to print
  * @b: the size of the memory to print
  * @n: inte
- * Return: aa
+ * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
