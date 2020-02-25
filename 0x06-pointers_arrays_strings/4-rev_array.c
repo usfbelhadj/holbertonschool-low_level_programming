1c8f74f5e3b83c0b9090dfe7638b643b9195a3cd
usfbelhadj <usf.belhadj@gmail.com>
2020-02-25 10:34:25 +0100

    aa

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
 /**
- *_strcmp - Compares two strings
+ *reverse_array- Compares two strings
  *@a :integer
  *@n :integer
  *Return: void
  */
 void reverse_array(int *a, int n)
 {
-	int j;
-	int temp;
-
-	for (j = 0; j < ((n - 1) / 2); j++)
-	{
-		temp = a[j];
-		a[j] = a[n - 1 - j];
-		a[n - 1 - j] = temp;
-	}
+int j, temp;
+for (j = 0; j < ((n - 1) / 2); j++)
+{
+temp = a[j];
+a[j] = a[n - 1 - j];
+a[n - 1 - j] = temp;
+}
 }
