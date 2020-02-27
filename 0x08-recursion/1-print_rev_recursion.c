85bb4f17aa6861298a69b1361b7cb3b9809bbe05
usfbelhadj <usf.belhadj@gmail.com>
2020-02-27 23:05:36 +0100

    aa

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_print_rev_recursion- prints a string rev
+ *@s: string
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+int i = 0;
+if (*s++ != '\0')
+if (s[i] != '\0')
+_print_rev_recursion(s);
+_putchar(s[i - 1]);
+if (s[0] != s[i])
+_putchar('\n');
+return;
+}
