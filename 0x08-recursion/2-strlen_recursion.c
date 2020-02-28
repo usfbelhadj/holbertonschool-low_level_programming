2dad1480bdeec56e5203c5851875e6454f44f3ac
usfbelhadj <usf.belhadj@gmail.com>
2020-02-28 20:20:05 +0100

    aa

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *_strlen_recursion- prints a string rev
+ *@s: string
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+return(1+_strlen_recursion(s+1));
+return(0);
+}
