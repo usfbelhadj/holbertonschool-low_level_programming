158827ec30c3ac60c55cd0141211d69e3872956b
usfbelhadj <usf.belhadj@gmail.com>
2020-03-01 21:03:43 +0100

    aa

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - change
+ * @argc: integer
+ * @argv: string
+ * Return: path
+ */
+int main(int argc, char *argv[])
+{
+if (argc == 1)
+printf("%s\n",argv[0]);
+return 0;
+}
