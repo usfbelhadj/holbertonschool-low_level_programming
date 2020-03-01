f1cba8fc3b5b31de41c351aa246a2dbc22baec67
usfbelhadj <usf.belhadj@gmail.com>
2020-03-01 21:47:50 +0100

    qq

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - change
+ * @argc: integer
+ * @argv: string
+ * Return: path
+ */
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc - 1);
+return (0);
+}
