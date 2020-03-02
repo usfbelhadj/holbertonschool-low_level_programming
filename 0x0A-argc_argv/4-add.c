286521c0451cf58069352270410f90db04b9b057
usfbelhadj <usf.belhadj@gmail.com>
2020-03-02 15:43:30 +0100

    aa

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - change
+ * @argc: integer
+ * @argv: string
+ * Return: path
+ */
+int main(int argc, char *argv[])
+{
+int i;
+int j;
+int sum = 0;
+
+if (argc == 1)
+{
+printf("0");
+}
+
+for (i = 1; argv[i]; i++)
+{
+for (j = 0; argv[i][j]; j++)
+{
+if (argv[i][j] < '0' || argv[i][j] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[i]);
+}
+
+printf("%d\n", sum);
+return (0);
+}
