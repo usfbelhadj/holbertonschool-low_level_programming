eb35cf0d66f1471ba31adab81f136c9b4ebb9434
usfbelhadj <usf.belhadj@gmail.com>
2020-03-03 20:26:23 +0100

    aa

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup- creates an array of chars
+ * @str: string
+ * Return: Nothing.
+ */
+char *_strdup(char *str)
+{
+char *s;
+unsigned int i = 0;
+unsigned int j;
+while (str[i] != '\0')
+{
+i++;
+}
+s = malloc(sizeof(*s) * i);
+if (s == NULL)
+return (NULL);
+else
+{
+for (j = 0; j < i; j++)
+s[j] = str[j];
+}
+return (s);
+}
