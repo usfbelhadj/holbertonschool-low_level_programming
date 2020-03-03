6ef4ad72f4add72798f5af52167b5a4fdcaa2d85
usfbelhadj <usf.belhadj@gmail.com>
2020-03-03 23:04:15 +0100

    str-concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat- creates an array of chars
+ * @s1: string
+ * Return: Nothing.
+ */
+char *str_concat(char *s1, char *s2)
+{
+int i = 0, j = 0, c1 = 0, c2 = 0, k;
+char *s;
+while (s1[i] != '\0')
+{
+i++;
+c1++;
+}
+while (s2[j] != '\0')
+{
+j++;
+c2++;
+}
+s = malloc(c1 + c2 + 1);
+i = 0;
+for (k = 0; k < c1; k++)
+{
+s[i] = s1[k];
+i++;
+}
+j = 0;
+for (k = 0; k < (c1 + c2); k++)
+{
+s[i] = s2[j];
+j++;
+i++;
+}
+return (s);
+}
