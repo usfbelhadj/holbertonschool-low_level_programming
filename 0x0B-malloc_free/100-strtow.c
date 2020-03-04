d7eff5dd85eaf436bb0eee432664d9b9771000cb
usfbelhadj <usf.belhadj@gmail.com>
2020-03-04 19:35:23 +0100

    strtow

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -10,24 +10,24 @@ char **strtow(char *str)
 {
 unsigned int i = 0;
 unsigned int j, k, l;
-char *s, **s1;
+char **s, *s1;
 while (str[i] != '\0')
 i++;
 if (i == 0)
 return (NULL);
-s = malloc(i * sizeof(str));
-if (s == NULL)
+s1 = malloc(sizeof(str) * i);
+if (s1 == NULL)
 return (NULL);
 for (j = 0; str[j]; j++)
 {
-if (s[j] != ' ')
-for (k = 0;str[k]; k++)
+if (s1[j] != ' ')
+for (k = 0; str[k]; k++)
 {
-s1 = s1 + s[k];
+s = s + s1[k];
 for (l = 0; s1[l]; l++)
-if (**s1 == ' ')
+if (**s == ' ')
 break;
 }
 }
-return (s1);
+return (s);
 }
