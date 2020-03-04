0062afaef10015da1d3dc167b4e1bbd04a363303
usfbelhadj <usf.belhadj@gmail.com>
2020-03-04 17:32:57 +0100

    grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid- creates an array of chars
+ * @width: intger
+ * @height: intger
+ * Return: Nothing.
+ */
+int **alloc_grid(int width, int height)
+{
+int i, j;
+int **arg;
+if (width <= 0 || height <= 0)
+return (NULL);
+arg = malloc(sizeof(int *)* height);
+if (arg == NULL)
+return (NULL);
+for (i = 0; i < height; i++)
+{
+arg[i] = malloc(sizeof(int *)* width);
+if (arg[i] == NULL)
+{
+for (j = 0 ; j < i; j++)
+{
+free (arg[i]);
+free (arg);
+return (NULL);
+}
+}
+}
+return (arg);
+}
