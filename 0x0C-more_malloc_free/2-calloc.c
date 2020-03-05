b225a95a8dcb8fd49264f4755be90d915b0c20da
usfbelhadj <usf.belhadj@gmail.com>
2020-03-05 23:51:06 +0100

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _calloc - malloc
+ *@nmemb: integer
+ *@size: integer
+ * Return: 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i, *p;
+if ( nmemb == 0 || size == 0)
+return (NULL);
+p = malloc (size * nmemb);
+if (p == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+p[i] = '\0';
+return (p);
+}
