9ccd0d1151a0017ceef79f9b4de83eb12371b57f
usfbelhadj <usf.belhadj@gmail.com>
2020-03-06 20:03:07 +0100

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i, *p;
-if (nmemb == 0 || size == 0)
+if (nmemb == 0 && size == 0)
 return (NULL);
 p = malloc(size * nmemb);
 if (p == NULL)
