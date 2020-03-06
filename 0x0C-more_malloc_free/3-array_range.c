4b0973f21565973fe0cb1c6fa65e5985642d31cc
usfbelhadj <usf.belhadj@gmail.com>
2020-03-06 21:36:39 +0100

    array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,7 +13,7 @@ int *array_range(int min, int max)
 int i, *arr, j = 0;
 if (min > max)
 return (NULL);
-arr = malloc(max - min);
+arr =malloc((max - min + 1) * sizeof(int));
 if (arr == NULL)
 return (NULL);
 for (i = min; min <= max; i++)
