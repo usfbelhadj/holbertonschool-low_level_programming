67b9a6856058c0430cb870387c6a4736b39e455f
usfbelhadj <usf.belhadj@gmail.com>
2020-03-10 11:16:25 +0100

    DOg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog - dog information
+ *@name: name of the dog
+ *@owner: the owner of the dog
+ *@age: the age of the dog
+ */
 struct dog
 {
 char *name;
