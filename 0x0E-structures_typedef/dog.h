8e1c220067ea4ec2eeba85533e971e58105e42b6
usfbelhadj <usf.belhadj@gmail.com>
2020-03-10 19:50:11 +0100

    Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,7 +12,7 @@ char *name;
 char *owner;
 float age;
 };
-typedef struct dog_t;
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
