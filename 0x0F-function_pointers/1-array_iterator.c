19f92c80c43139b72ef34959c19b3c6c50c89af5
usfbelhadj <usf.belhadj@gmail.com>
2020-03-11 17:42:02 +0100

    Array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
 #include <stdio.h>
+/**
+ * array_iterator - to print a name
+ * @action: The function i use to check the array
+ * @array: an array
+ * @size: the size of an array
+ * Return : Void
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
