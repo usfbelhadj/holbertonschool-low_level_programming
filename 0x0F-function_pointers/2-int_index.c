0d05755621f8565defe976fc79cb9353058a92d2
usfbelhadj <usf.belhadj@gmail.com>
2020-03-11 19:26:12 +0100

    index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size <= 0 || array == NULL)
+return (-1);
+for (i = 0; size > i; i++)
+if (cmp(array[i]) != 0)
+return (i);
+return (-1);
+}
