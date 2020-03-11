f4dc033d68995fe03032d67bdc3ad80ba9171fe6
usfbelhadj <usf.belhadj@gmail.com>
2020-03-11 17:43:35 +0100

    Array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int i;
-if (array == NULL)
+if (array == NULL || size == NULL || action == NULL)
 return;
 for (i = 0; size > i; i++)
 {
