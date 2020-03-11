1895668d3f183832b0f03321b29fcd096de80124
usfbelhadj <usf.belhadj@gmail.com>
2020-03-11 15:23:21 +0100

    Func&Ptr

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - to print a name
+ * @f: The function i use to print the name
+ * @name: a name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name != NULL)
+f(name);
+}
