9a8bebc4bc1f3ff55c1083d840d5f70cbfb35dbe
usfbelhadj <usf.belhadj@gmail.com>
2020-03-12 23:49:02 +0100

    Test

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - sums
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+/**
+ * op_sub - substracts
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+/**
+ * op_mul - multiplies
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+/**
+ * op_div - divises
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+/**
+ * op_mod - rest int
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
