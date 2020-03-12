2b08d68325339f67e9c1c9f7e20cea3196fa4c82
usfbelhadj <usf.belhadj@gmail.com>
2020-03-13 00:00:45 +0100

    Test

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,10 +7,10 @@
  * @argc: integer
  * Return: the result
  */
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 int n, nb1, nb2;
-int (*p) (int, int);
+int (*p)(int, int);
 if (argc != 4)
 {
 printf("Error\n");
@@ -29,7 +29,7 @@ if (p == NULL)
 printf("Error\n");
 exit(99);
 }
-n =(p)(nb1, nb2);
+n = (p)(nb1, nb2);
 printf("%i\n", n);
 return (0);
 }
