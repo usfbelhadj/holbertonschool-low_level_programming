f1d2526db9e9879a50f30c3ebe76cf6fcd7103b8
usfbelhadj <usf.belhadj@gmail.com>
2020-03-14 16:43:32 +0100

    Test

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,10 +10,10 @@ int sum_them_all(const unsigned int n, ...)
 {
 unsigned int i;
 int sum = 0;
-if (n == 0)
-return (0);
 va_list(count);
 va_start(count, n);
+if (n == 0)
+return (0);
 for (i = 0; i < n; i++)
 sum += va_arg(count, unsigned int);
 va_end(count);
