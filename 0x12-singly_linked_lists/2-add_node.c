1dcad12248d45438b6b76850cabd43f120b4fab3
usfbelhadj <usf.belhadj@gmail.com>
2020-03-21 15:43:09 +0100

    Fix-Add-Node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,14 +9,14 @@
 list_t *add_node(list_t **head, const char *str)
 {
 struct list_s *temp;
-int i;
+unsigned int i;
 temp = (struct list_s *) malloc(sizeof(struct list_s));
 if (temp == NULL)
 return (NULL);
 if (temp != NULL)
 {
 temp->str = strdup(str);
-for (i = 0; i < str[i]; i++)
+for (i = 0;str[i] != '\0'; i++)
 ;
 temp->next = (*head);
 temp->len = i;
