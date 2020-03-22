f6045286b0bf8019b8bedb335c591309b942d7ee
usfbelhadj <usf.belhadj@gmail.com>
2020-03-22 15:07:05 +0100

    Fixx ForFun

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-void beFore (void) __attribute__ ((constructor)); 
-void beFore (void) 
-{ 
-    printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n"); 
-} 
-  
+/**
+ * beFore- Functions that are executed before main
+ *
+ */
+void beFore(void) __attribute__ ((constructor));
+void beFore(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
