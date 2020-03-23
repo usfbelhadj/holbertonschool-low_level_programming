20cb43b272262a2d9b3710dfb2590a221487a070
usfbelhadj <usf.belhadj@gmail.com>
2020-03-23 21:26:51 +0100

    Add_Node_End

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,10 @@
 #include "lists.h"
-
-
+/**
+ *add_nodeint_end- Adds a new node at the End
+ * @head:The start of the Node
+ * @n:The new Node
+ * Return: Integer
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *temp, *newNode;
