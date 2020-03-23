9f5b460b2aa21c143f0c31f608d10d0f13f96487
usfbelhadj <usf.belhadj@gmail.com>
2020-03-23 02:04:33 +0100

    Len-of-a-list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len- Returns the number of elements in a linked
+ *@h:Struct Pointer
+ *Return: Integer
+ */
+
+size_t listint_len(const listint_t *h)
+{
+int i;
+while (h != NULL)
+{
+h = h->next;
+i++;
+}
+return (i);
+}
