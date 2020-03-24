547d0bdaa9bf6d5ec4fd7dbd5e44a4088574d04a
usfbelhadj <usf.belhadj@gmail.com>
2020-03-24 13:20:36 +0100

    Fiix pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,15 +1,19 @@
 #include "lists.h"
-
+/**
+ *pop_listint- Delete List
+ * @head:First iteam in the list
+ */
 int pop_listint(listint_t **head)
 {
-listint_t *temp = *head;
+listint_t *temp;
 int l;
+temp = *head;
 if (temp == NULL)
 return (0);
-while (*head != NULL)
+while ((*head)->next != NULL)
 {
-l = (*head)->n;
 *head = (*head)->next;
+l = (*head)->n;
 free(temp);
 }
 return (l);
