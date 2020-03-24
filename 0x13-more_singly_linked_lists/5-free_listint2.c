6a5e3fa7b33f9f91a8ca9f9b67bf10830b41547e
usfbelhadj <usf.belhadj@gmail.com>
2020-03-24 14:13:52 +0100

    Fiix Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,11 +6,11 @@
 void free_listint2(listint_t **head)
 {
 listint_t *temp;
-while (*head)
+while (*head != NULL)
 {
 temp = *head;
 *head = (*head)->next;
 free(temp);
 }
-*head = NULL;
+head = NULL;
 }
