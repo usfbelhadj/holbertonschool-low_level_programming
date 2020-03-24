e222739781eb3e9be84a81b578660c020434380a
usfbelhadj <usf.belhadj@gmail.com>
2020-03-24 21:20:17 +0100

    Fiix

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,11 +7,11 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+unsigned int i = 0;
 listint_t *temp;
 temp = head;
 if (temp == NULL)
-return (0);
-unsigned int i = 0;
+return (NULL);
 while (temp != NULL)
 {
 if (i == index)
