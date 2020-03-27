b934d635f48b940be32be375178589fb4dfe0711
usfbelhadj <usf.belhadj@gmail.com>
2020-03-27 17:52:54 +0100

    Clear

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * clear_bit - set bit to 0 at
+ *@n: unsigned long integer
+ *@index: bit
+ *Return: integer
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(unsigned long int) * 8)
+	return (-1);
+	*n = *n & 1 << index;
+	return (1);
+}
