d11c7ffdd55f4a09daff179055c84a38414ba004
usfbelhadj <usf.belhadj@gmail.com>
2020-03-31 19:58:21 +0100

    CFile_FIX

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,10 +10,10 @@ int create_file(const char *filename, char *text_content)
 	int i = 0, op, w;
 
 	if (filename == NULL)
-		return (-1);
+	return (-1);
 	op = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 	if (op == -1)
-		return (-1);
+	return (-1);
 	if (text_content != NULL)
 	{
 		while (text_content[i] != '\0')
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	w = write(op, text_content, i);
 	if (w == -1)
-		return (-1);
+	return (-1);
 	close(op);
 	return (1);
 }
