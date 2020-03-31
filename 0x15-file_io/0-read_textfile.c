42302e2975610fac98b33bdf741febe1247931c3
usfbelhadj <usf.belhadj@gmail.com>
2020-03-31 19:26:02 +0100

    Done

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * read_textfile - Read File
+ * @filename: file name
+ * @letters: Size of file
+ * Return: pointer.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t w;
+int op, r;
+char *c;
+
+	if (filename == NULL)
+		return (0);
+	c = malloc(letters);
+	if (!c)
+		return (0);
+	op = open(filename, O_RDWR);
+	if (op == -1)
+		return (0);
+	r = read(op, c, letters);
+	if (r == -1)
+		return (0);
+
+	w = write(1, c, r);
+	if (w == -1)
+		return (0);
+	free(c);
+	close(op);
+
+return (w);
+}
