9f629b7964d7b67956c7a4a8a2872e3007003855
usfbelhadj <usf.belhadj@gmail.com>
2020-03-31 23:32:07 +0100

    Append Fiiix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 /**
- * 
- * 
- * 
- * 
+ *append_text_to_file- Append file
+ *@filename: file name
+ *@text_content: content file
+ *Return: pointer
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+
 	int op, w, i = 0;
-	
+
 	if (filename == NULL)
 	return (-1);
-	op = open(filename, O_CREAT | O_TRUNC | O_APPEND);
+	op = open(filename, O_RDWR | O_APPEND);
 	if (op == -1)
 	return (-1);
 	if (text_content != NULL)
@@ -24,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	w = write(op, text_content, i);
 	if (w == -1)
 	return (-1);
-	
+
 	close(op);
 	return (1);
 }
