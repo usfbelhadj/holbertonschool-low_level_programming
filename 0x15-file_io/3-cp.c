fb2022fceaa96253faeff7098d72ba39ef0102e1
usfbelhadj <usf.belhadj@gmail.com>
2020-04-02 00:26:54 +0100

    Fiix copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,13 +15,13 @@ int main(int argc, char **argv)
         dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
         exit(97);
     }
-    ff = open(argv[1], O_RDWR, 0644);
+    ff = open(argv[1], O_RDONLY);
     if (ff == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
         exit(98);
     }
-    ft = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0644);
+    ft = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0644);
     if (ft == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
