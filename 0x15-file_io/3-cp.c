352eb27c58bc7c7af5bc1624c0cd9040ba0f2e13
usfbelhadj <usf.belhadj@gmail.com>
2020-04-02 00:23:33 +0100

    Fiix copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,47 +12,52 @@ int main(int argc, char **argv)
     char *buf[1024];
     if (argc != 3)
     {
-    dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-    exit(97);
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
     }
     ff = open(argv[1], O_RDWR, 0644);
     if (ff == -1)
     {
-    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-    exit(98);
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
     }
     ft = open(argv[2], O_CREAT | O_TRUNC | O_RDWR, 0644);
     if (ft == -1)
     {
-    dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-    exit(99);
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        exit(99);
     }
     n = read(ff, buf, 1024);
     if (n == -1)
     {
-    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-    exit(98);
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+        exit(98);
     }
     while (n != 0)
     {
-    w = write(ft, buf, n);
-    if (w == -1)
-    {
-    dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-    exit(99);
-    }
-    n = read(ff, buf, 1024);
-    if (n == -1)
-    {
-    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
-    exit(98);
-    }
+        w = write(ft, buf, n);
+        if (w == -1)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            exit(99);
+        }
+        n = read(ff, buf, 1024);
+        if (n == -1)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
+            exit(98);
+        }
     }
     ff = close(ff);
-    ft = close(ft);
-    if ((ff == -1) | (ft == -1))
+    if (ff == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ff);
+        exit(100);
+    }
+    ft = close(ft);
+    if (ft == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", ft);
         exit(100);
     }
     return (0);
