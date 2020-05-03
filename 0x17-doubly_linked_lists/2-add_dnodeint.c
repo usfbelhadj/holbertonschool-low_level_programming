7dba0fb81a2c63589e3773fee77db087564e02c8
usfbelhadj <usf.belhadj@gmail.com>
2020-05-03 17:15:18 +0100

    Add

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *add_nodeint- Adds a new node at the beginning
+ * @head:The start of the Node
+ * @n:The new Node
+ * Return: Integer
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *temp;
+temp = malloc(sizeof(dlistint_t));
+if (temp == NULL)
+return (NULL);
+if (temp != NULL)
+{
+temp->n = n;
+temp->next = *head;
+*head = temp;
+}
+return (temp);
+}
