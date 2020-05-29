a32bf34215c3e85e5dc2e726feed9dd5210c64ae
usfbelhadj <usf.belhadj@gmail.com>
2020-05-29 20:26:09 +0100

    Test1

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table.
+ * @size : size of the hash table
+ * Return: pointer to the hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+hash_table_t *hash_t;
+
+if (!size)
+{
+return (NULL);
+}
+hash_t = malloc(sizeof(hash_table_t));
+if (!hash_t)
+{
+return (NULL);
+}
+
+hash_t->size = size;
+hash_t->array = malloc(sizeof(hash_t->array) * size);
+if (!hash_t)
+{
+return (NULL);
+}
+return (hash_t);
+
+}
