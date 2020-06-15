853b83c6db12bc4d3ad9e5d12625e0b72f0d281d
usfbelhadj <usf.belhadj@gmail.com>
2020-06-15 16:38:53 +0100

    push

---
+++ sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * 
+ * 
+ * 
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+    int tmp;
+    size_t j, i = 0;
+
+    if (!array || !size)
+		return;
+    while( i < size)
+    {
+        for (j = 0; j < size - 1; j++)
+        {
+            if (array[j] > array[j+1])
+            {
+                tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+            }
+        }
+        i++;
+    }
+
+}
